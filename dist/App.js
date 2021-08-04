0a00c132e7d89a42a24d111c8210032c34bd76e2
smallappsclub <smallappsclub@users.noreply.github.com>
2021-08-04 23:07:23 +0000

    deploy: 3d4b3683be9ed977b916efbabbe970928339cf73

---
+++ dist/App.js
@@ -0,0 +1,5 @@
+import React from "../_snowpack/pkg/react.js";
+function App() {
+  return /* @__PURE__ */ React.createElement(React.Fragment, null, /* @__PURE__ */ React.createElement("header", null, /* @__PURE__ */ React.createElement("h1", null, "Welcome to smallapps", /* @__PURE__ */ React.createElement("i", null, "."), "club !")), /* @__PURE__ */ React.createElement("main", null, /* @__PURE__ */ React.createElement("p", null, "Site under construction")));
+}
+export default App;
